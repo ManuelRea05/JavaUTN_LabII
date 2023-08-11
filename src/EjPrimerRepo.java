5a3e7afca734960de828ef5f795f49d7fab79bc8
ManuRea1 <manuelrea97@gmail.com>
2023-08-11 12:11:00 -0300

    EjercicioRepositorio

--- src/EjPrimerRepo.java
+++ src/EjPrimerRepo.java
@@ -3,23 +3,21 @@ import java.util.Scanner;
 public class EjPrimerRepo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num1, num2, num3 , aux = 0;
-        System.out.println("Ingrese un número: ");
-        num1 = scanner.nextInt();
-        System.out.println("Ingrese otro número: ");
-        num2 = scanner.nextInt();
-        System.out.println("Ingrese otro número: ");
-        num3 = scanner.nextInt();
+        System.out.print("Ingresa el primer número: ");
+        int num1 = scanner.nextInt();
 
-        if (num2 > num1){
-            num2 = aux;
-            num1 = num2;
-            num2 = aux;
-        } else if (num3 > num2){
-            num3 = aux;
-            num3 = num2;
-            num3 = aux;
-        }
-        
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Ingresa el tercer número: ");
+        int num3 = scanner.nextInt();
+
+        int mayor = Math.max(Math.max(num1, num2), num3);
+
+        int menor = Math.min(Math.min(num1, num2), num3);
+
+        int medio = num1 + num2 + num3 - mayor - menor;
+
+        System.out.println("Números ordenados: " + mayor + ", " + medio + ", " + menor);
     }
 }
