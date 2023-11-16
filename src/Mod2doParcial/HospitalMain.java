17f1e3e27f94154ded8fe7fc0914b47443dabaab
ManuelRea05 <agustinrea105@gmail.com>
2023-11-15 21:47:57 -0300

    mod2parcialterminado

--- src/Mod2doParcial/HospitalMain.java
+++ src/Mod2doParcial/HospitalMain.java
@@ -65,8 +65,61 @@ class Hospital {
         // Agregar el paciente a la base de datos
         String consulta = "INSERT INTO pacientes (nombre, edad, historial_medico, doctor, fecha_ingreso) VALUES ('" + paciente.getNombre() + "', " + paciente.getEdad() + ", '" + paciente.getHistorialMedico() + "', " + paciente.getDoctorC() + ", '" + paciente.getFechaIngreso() + "')";
         DBHelper.ejecutarConsulta(consulta);
+        System.out.println("Paciente agregado correctamente");
+    }
+
+    public void eliminarPaciente(String nombrePaciente) {
+        String consulta = "DELETE FROM pacientes WHERE `pacientes`.`nombre` = '"+nombrePaciente+"'";
+        DBHelper.ejecutarConsulta(consulta);
+        System.out.println("Paciente "+nombrePaciente+" eliminado correctamente");
+    }
+
+    public void mostrarListaPacientes() throws SQLException {
+        String consulta = "SELECT * FROM `pacientes`";
+        ResultSet resultado = DBHelper.ejecutarConsultaConResultado(consulta);
+
+        // Imprime una cabecera de columnas para los datos de los estudiantes.
+        System.out.println("ID\tNombre\tEdad\tHistorial Medico\tFecha Ingreso\tDoctor");
+
+        listarPacientes(resultado);
+        resultado.close();
+    }
+
+    public void asignarDoctorCabecera(String nombreDoctor, String nombrePaciente) throws SQLException {
+        String consulta = "UPDATE pacientes SET doctor = (SELECT id FROM doctores WHERE nombre = '"+nombreDoctor+"') WHERE nombre = '"+nombrePaciente+"'";
+        DBHelper.ejecutarConsulta(consulta);
+
+    }
+
+    public void mostrarPacientesEntreFechas(String fechaInicio, String fechaFin) {
+        String consulta = "SELECT * FROM `pacientes` WHERE `fecha_ingreso` BETWEEN '"+fechaInicio+"' AND '"+fechaFin +"'";
+        ResultSet resultado = DBHelper.ejecutarConsultaConResultado(consulta);
+        listarPacientes(resultado);
+    }
+
+    public void listarPacientes(ResultSet resultado){
+        if (resultado != null) {
+            try {
+                System.out.println("Lista de Pacientes:");
+                System.out.printf("%-10s %-15s %-5s %-20s %-12s %-10s\n", "ID", "Nombre", "Edad", "Historial Médico", "Fecha Ingreso", "Doctor");
+
+                while (resultado.next()) {
+                    int id = resultado.getInt("id");
+                    String nombre = resultado.getString("nombre");
+                    int edad = resultado.getInt("edad");
+                    String historialMedico = resultado.getString("historial_medico");
+                    Date fechaIngreso = resultado.getDate("fecha_ingreso");
+                    int idDoctor = resultado.getInt("doctor");
+
+                    System.out.printf("%-10d %-15s %-5d %-20s %-12s %-10d\n", id, nombre, edad, historialMedico, fechaIngreso, idDoctor);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
 
 class DBHelper {
         private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
@@ -84,7 +137,6 @@ class DBHelper {
                 // Ejecutar la consulta
                 statement.executeUpdate();
             }
-
             // Cerrar la conexión
             connection.close();
         } catch (SQLException e) {
@@ -111,14 +163,19 @@ class DBHelper {
 }
 
 class HospitalMain{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Hospital hospital = new Hospital();
 
         Date fechaActual = new Date(2023 - 1900, 1 - 1, 10);
 
         Paciente paciente1 = new Paciente("Manuel", 45, "Ninguno", 1, fechaActual);
-
-        hospital.agregarPaciente(paciente1);
+        Paciente paciente2 = new Paciente("Santiago", 45, "Sufrio de desamor", 1, fechaActual);
+        // hospital.agregarPaciente(paciente1);
+        // hospital.mostrarListaPacientes();
+       // hospital.mostrarPacientesEntreFechas("2023-01-10", "2023-03-10")
+        //hospital.eliminarPaciente("Santiago");
+        //hospital.agregarPaciente(paciente2);
+        hospital.asignarDoctorCabecera("Doctor2", "Santiago");
     }
 }
 
